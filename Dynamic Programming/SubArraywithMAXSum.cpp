4a2f313caca170d37e7a6cf8bf0db6d70e332565
Anuj Shaan <anujprakashshaan@gmail.com>
2020-08-29 12:30:08 +0530

    subArrayWithMaxValue.cpp

--- Dynamic Programming/SubArraywithMAXSum.cpp
+++ Dynamic Programming/SubArraywithMAXSum.cpp
@@ -6,6 +6,7 @@ Given an array of driver's ratings over N days.
 Value over a period = (Contiuous sum * Minimum rating in that period)
 
 eg: [3, 1, 6, 4, 5, 2]
+
 Value could be (3+1+6+4+5+2) * 1 = 21.
 
 Find the maximum value.
