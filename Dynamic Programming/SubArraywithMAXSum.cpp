e0c35f2c00f47b2ebd8e8fdac662d485999e15e8
Anuj Shaan <anujprakashshaan@gmail.com>
2020-08-29 12:28:36 +0530

    Update SubArraywithMAXSum.cpp

--- Dynamic Programming/SubArraywithMAXSum.cpp
+++ Dynamic Programming/SubArraywithMAXSum.cpp
@@ -1,3 +1,18 @@
+/*
+this question is asked in Amazon and Uber SDE-1
+online assessment.
+
+Given an array of driver's ratings over N days.
+Value over a period = (Contiuous sum * Minimum rating in that period)
+
+eg: [3, 1, 6, 4, 5, 2]
+Value could be (3+1+6+4+5+2) * 1 = 21.
+
+Find the maximum value.
+Here max value is (6+4+5) * 4 = 60.
+
+O(N^2) won't pass most test cases.
+*/
 #include<bits/stdc++.h>
 using namespace std;
 
